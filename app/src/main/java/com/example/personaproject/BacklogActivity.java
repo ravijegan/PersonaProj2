ab7813bfd7a5443bdafade0ffe7df554cdb4a394
ravijegan <ravijeganuntthan@gmail.com>
2022-04-27 23:07:40 -0400

    navigation to different activities

--- app/src/main/java/com/example/personaproject/BacklogActivity.java
+++ app/src/main/java/com/example/personaproject/BacklogActivity.java
@@ -1,7 +1,10 @@
 package com.example.personaproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BacklogActivity extends AppCompatActivity {
@@ -12,4 +15,35 @@ public class BacklogActivity extends AppCompatActivity {
         setContentView(R.layout.backlog);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()){
+            case R.id.homeMenu:{
+                System.out.println("testing");
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.createMenu:{
+                System.out.println("testing 2");
+                Intent intent = new Intent(this, CreateActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.backlogMenu:{
+                System.out.println("testing 3");
+                Intent intent = new Intent(this, BacklogActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.todayMenu:{
+                System.out.println("testing 4");
+                Intent intent = new Intent(this,DailyActivity.class);
+                startActivity(intent);
+                break;
+            }
+        }
+        return true;
+    }
 }
--- app/src/main/java/com/example/personaproject/CreateActivity.java
+++ app/src/main/java/com/example/personaproject/CreateActivity.java
@@ -1,7 +1,10 @@
 package com.example.personaproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CreateActivity extends AppCompatActivity {
@@ -10,5 +13,37 @@ public class CreateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_task);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()){
+            case R.id.homeMenu:{
+                System.out.println("testing");
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.createMenu:{
+                System.out.println("testing 2");
+                Intent intent = new Intent(this, CreateActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.backlogMenu:{
+                System.out.println("testing 3");
+                Intent intent = new Intent(this, BacklogActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.todayMenu:{
+                System.out.println("testing 4");
+                Intent intent = new Intent(this,DailyActivity.class);
+                startActivity(intent);
+                break;
+            }
+        }
+        return true;
     }
 }
